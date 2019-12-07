b2e4bddcef9b46bf8368847d9279297d97eed1fb
JeffreyAbbinante <jefflukas2008@gmail.com>
2019-12-07 02:22:35 -0500

    upload config.h for future configurable version

---
+++ config.h
@@ -0,0 +1,34 @@
+/*
+ * astatusbar is a statusbar replacement for dwm
+ * config.h: configuration file for astatbar
+ *
+ * Copyright (c) 2019 Jeffrey Abbinante
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * Implements the very basic elements of a statusbar.
+ * Tested on ThinkPad A485.
+ */
+
+#ifndef ASTATBAR_CONFIG_H
+#define ASTATBAR_CONFIG_H
+
+//uncomment this to set astatbar in laptop mode, desktop mode otherwise
+//#define LAPTOP
+
+//uncomment this to set astatbar in dual battery mode, single battery mode otherwise (depends on laptop mode being enabled)
+//#define DUAL_BAT
+
+//uncomment this to set astatbar in amdgpu mode, intelgpu mode otherwise (depends on laptop mode being enabled)
+//#define AMDGPU
+
+#endif
